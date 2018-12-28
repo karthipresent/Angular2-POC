380032925b2cd7a0d627794a211146967bb80988
karthipresent <karthipresent@gmail.com>
2018-12-28 17:43:18 +0530

    Update login-in.component.ts

--- src/app/login-in/login-in.component.ts
+++ src/app/login-in/login-in.component.ts
@@ -4,8 +4,7 @@ import { Router } from '@angular/router';
  
 @Component({
   selector: 'app-login-in',
-  templateUrl: './login-in.component.html',
-  styleUrls: ['./login-in.component.scss']
+  templateUrl: './login-in.component.html'
 })
 export class LoginInComponent implements OnInit {
   constructor(private giftCartService: GiftCartService, private router: Router) { }
