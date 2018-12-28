1d377a101834924f86b6f78a32d24f2a25e97d63
karthipresent <karthipresent@gmail.com>
2018-12-28 17:43:46 +0530

    Update logout.component.ts

--- src/app/logout/logout.component.ts
+++ src/app/logout/logout.component.ts
@@ -3,8 +3,7 @@ import { GiftCartService } from '../gift-cart.service';
 
 @Component({
   selector: 'app-logout',
-  templateUrl: './logout.component.html',
-  styleUrls: ['./logout.component.sass']
+  templateUrl: './logout.component.html'
 })
 export class LogoutComponent implements OnInit {
 
